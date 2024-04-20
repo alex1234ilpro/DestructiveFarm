5b2e7d611522b8be4846a4209d3cef6e7b9240b3
Alex1234ilpro <aseroalessandro5@gmail.com>
2024-04-21 00:01:28 +0200

    chore: Update config.py default settings

--- server/config.py
+++ server/config.py
@@ -5,19 +5,19 @@ CONFIG = {
     # fetch FLAG_FORMAT from sploits' stdout.
 
     # WARNING: THIS IS A FALLBACK DICTIONARY IF THE PROTOCOL DOES NOT PROVIDE UPDATED TEAMS.
-    'TEAMS': {'Team #{}'.format(i): '10.0.0.{}'.format(i) for i in range(1, 29 + 1)},
+    'TEAMS': {'Team {}'.format(i): '10.60.{}.1'.format(i) for i in range(1, 87)},
 
         
-    'FLAG_FORMAT': r'[a-zA-Z0-9]{31}=',
+    'FLAG_FORMAT': r'[A-Z0-9]{31}=',
 
     # This configures how and where to submit flags.
     # The protocol must be a module in protocols/ directory.
 
     'SYSTEM_PROTOCOL': 'dummy',
     # 'ATTACK_INFO_ENDPOINT': "http://#FIXME",
-    # 'SYSTEM_HOST': '127.0.0.1',
-    # 'SYSTEM_PORT': 31337,
-    # 'TEAM_TOKEN': 'your_secret_token',
+    'SYSTEM_HOST': '10.10.0.1',
+    'SYSTEM_PORT': 8080,
+    'TEAM_TOKEN': 'your_secret_token',
 
     # The server will submit not more than SUBMIT_FLAG_LIMIT flags
     # every SUBMIT_PERIOD seconds. Flags received more than
@@ -25,7 +25,7 @@ CONFIG = {
 
     'SUBMIT_FLAG_LIMIT': 250,
     'SUBMIT_PERIOD': 60,
-    'FLAG_LIFETIME': 11 * 60,
+    'FLAG_LIFETIME': 10 * 60,
 
     # Password for the web interface. You can use it with any login.
     # This value will be excluded from the config before sending it to farm clients.
